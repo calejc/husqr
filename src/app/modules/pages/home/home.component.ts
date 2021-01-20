e39124f5b49a0340ef40e4295e164a8bd57b7b4b
James Wiesenthal <jameswiesenthal@mail.mccneb.edu>
2021-01-19 21:43:49 -0600

    beginning user and husq firebase storeage

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -5,6 +5,9 @@ import { Post } from 'src/app/core/data/types/post.interface';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { HusqFormComponent } from 'src/app/modules/components/husq-form/husq-form.component';
 import { Observable} from 'rxjs';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import firebase from 'firebase/app';
+import { AngularFirestore } from '@angular/fire/firestore'
 
 @Component({
   selector: 'app-home',
@@ -14,7 +17,7 @@ import { Observable} from 'rxjs';
 export class HomeComponent implements OnInit {
   newHusqForm: FormGroup;
   post$: Observable<Post[]>;
-  constructor(public service: TimelineService, public dialog: MatDialog) { 
+  constructor(public service: TimelineService, public dialog: MatDialog, public auth: AuthenticationService, public firestore: AngularFirestore) { 
   }
 
   ngOnInit(): void {
@@ -22,25 +25,44 @@ export class HomeComponent implements OnInit {
 
   
 
-  newHusq(){
-    const config = new MatDialogConfig();
-    config.autoFocus = true;
-    const dr = this.dialog.open(HusqFormComponent, config);
-    dr.afterClosed().subscribe(result => {
-      let post: Post = {
-        postId: 101,
-        userId: 3212,
-        displayName: "Cale",
-        username: "cjcortney",
-        avatar: "",
-        datetime: new Date().toLocaleString(),
-        post: result.post,
-        likes: 5,
-        isReply: false,
-        replies: []
-      }
-      this.service.addPost(post);
-    });
+  // newHusq(){
+  //   const config = new MatDialogConfig();
+  //   config.autoFocus = true;
+  //   const dr = this.dialog.open(HusqFormComponent, config);
+  //   dr.afterClosed().subscribe(result => {
+  //     let post: Post = {
+  //       postId: 101,
+  //       userId: this.auth.userState.uid,
+  //       displayName: this.auth.userState.displayName,
+  //       username: "",
+  //       avatar: this.auth.userState.photoURL,
+  //       datetime: new Date().toLocaleString(),
+  //       post: result.post,
+  //       likes: 0,
+  //       isReply: false,
+  //       replies: []
+  //     }
+  //     this.service.addPost(post);
+  //   });
+  // }
+
+  temp() {
+    this.firestore.collection("users").doc(this.auth.userState.uid).update({
+      
+      "replies": [1,2,3]
+    })
+  }
+
+  newHusq() {
+    this.firestore.collection('users', ref => 
+      ref.where("uid", "==", this.auth.userState.uid)).get().subscribe(ss => { 
+        if (ss.docs.length === 0) {
+          console.log("not found")
+        } else {
+          ss.docs.forEach(doc => 
+            {console.log(doc.data())})
+          }
+        })
   }
 
 }
