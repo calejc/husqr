d06783aba3db223062e9f37aa7031018f4a731f4
James Wiesenthal <jameswiesenthal@mail.mccneb.edu>
2021-01-30 09:56:35 -0600

    new husq form styling

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -40,6 +40,9 @@ export class HomeComponent implements OnInit {
   newHusq(){
     const config = new MatDialogConfig();
     config.autoFocus = true;
+    config.disableClose = false;
+    config.minWidth = 250;
+    config.minHeight = 250;
     const dr = this.dialog.open(HusqFormComponent, config);
     dr.afterClosed().subscribe(result => {
       let post: Post = {
