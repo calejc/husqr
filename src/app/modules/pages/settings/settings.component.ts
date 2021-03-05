9caf10954e60411049ade3546cf4a8656e5c24e9
Cale Cortney <thortney@mailbox.org>
2021-03-04 20:26:26 -0600

    settings page

--- src/app/modules/pages/settings/settings.component.ts
+++ src/app/modules/pages/settings/settings.component.ts
@@ -8,6 +8,7 @@ import {
   FormControl,
 } from '@angular/forms';
 import { Observable, Subject, of } from 'rxjs';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { FirestoreService } from 'src/app/core/services/firestore.service';
 import { map } from 'rxjs/operators';
 
@@ -18,8 +19,11 @@ import { map } from 'rxjs/operators';
 })
 export class SettingsComponent implements OnInit {
 
-
+  preview: string | ArrayBuffer;
+  selectedImage: any;
+  profilePicture: File;
   photoURL: string;
+  fileUpload: File;
   displayName: string;
   user: any;
   id: string;
@@ -30,6 +34,7 @@ export class SettingsComponent implements OnInit {
   constructor(
     public authenticationService: AuthenticationService, 
     public firestoreService: FirestoreService,
+    public afStorage: AngularFireStorage,
     private fb: FormBuilder) {
   }
 
@@ -43,7 +48,8 @@ export class SettingsComponent implements OnInit {
       this.bio = user.bio ?  user.bio : '';
 
       this.settingsForm = this.fb.group({
-        photoURL: new FormControl(this.photoURL, [Validators.minLength(6)]),
+        profilePicture: new FormControl(),
+        // photoURL: new FormControl(this.photoURL, [Validators.minLength(6)]),
         displayName: new FormControl(this.displayName, [Validators.required, Validators.minLength(3)]),
         bio: new FormControl(this.bio, [Validators.maxLength(150)])
       });
@@ -55,25 +61,44 @@ export class SettingsComponent implements OnInit {
     
   }
 
-  
+  fileSelect($event){
+    const reader = new FileReader()
+    reader.onload = (loadEvent) => (this.preview = loadEvent.target.result);
+    console.log("preview", this.preview);
+    reader.readAsDataURL($event.target.files[0])
+    this.selectedImage = $event.target.files[0]
+  }
 
   saveSettings(){
-    let settings = {
-      "displayName": this.displayName ? this.displayName : '',
-      "photoURL": this.photoURL ? this.photoURL : '',
-      "bio": this.bio ? this.bio : ''
-    }
-  //   this.firestoreService.collectionRefs.usersRef.doc(this.id).update(settings);
-  // }
-  
-    if (this.settingsForm.valid) {
-      let settings = {
-        "photoURL": this.settingsForm.controls.photoURL.value ? this.settingsForm.controls.photoURL.value : '', 
-        "displayName": this.settingsForm.controls.displayName.value ? this.settingsForm.controls.displayName.value : '', 
-        "bio": this.settingsForm.controls.bio.value ? this.settingsForm.controls.bio.value : ''
-      }
-      this.firestoreService.updateUserSettings({items: settings, ref: this.firestoreService.collectionRefs.usersRef, docId: this.id});
-    }
+    console.log(this.settingsForm.controls)
+    // let fileToUpload = reader.readAsDataURL(this.settingsForm.get("profilePicture").value)
+    let fileToUpload = this.selectedImage;
+    // console.log("uploadFile: ", fileToUpload);
+    // console.log("type", typeof(fileToUpload));
+    let path = `${this.user.uid}_${Date.now()}`
+    this.afStorage.upload(path, fileToUpload).snapshotChanges().subscribe((s) => {
+      console.log(s.state);
+      console.log(s.bytesTransferred);
+      console.log(s.ref.getDownloadURL())
+    })
+    
+    // .pipe((s) => {
+      // this.afStorage.ref(path).getDownloadURL()
+    // })
+
+    // let settings = {
+    //   "displayName": this.displayName ? this.displayName : '',
+    //   // "photoURL": this.photoURL ? this.photoURL : '',
+    //   "bio": this.bio ? this.bio : ''
+    // }
+    // if (this.settingsForm.valid) {
+    //   let settings = {
+    //     // "photoURL": this.settingsForm.controls.photoURL.value ? this.settingsForm.controls.photoURL.value : '', 
+    //     "displayName": this.settingsForm.controls.displayName.value ? this.settingsForm.controls.displayName.value : '', 
+    //     "bio": this.settingsForm.controls.bio.value ? this.settingsForm.controls.bio.value : ''
+    //   }
+    //   this.firestoreService.updateUserSettings({items: settings, ref: this.firestoreService.collectionRefs.usersRef, docId: this.id});
+    // }
   }
 
 }
